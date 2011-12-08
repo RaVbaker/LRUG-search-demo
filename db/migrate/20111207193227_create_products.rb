71674088b8ce797603e629c4a6c9b7cee970c2cf
Rafal Piekarski <ravbaker@gmail.com>
2011-12-08 12:14:15 +0100

    products model

--- db/migrate/20111207193227_create_products.rb
+++ db/migrate/20111207193227_create_products.rb
@@ -1,7 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.text :description
+      t.string :producer_name
+      t.decimal :price, precision: 2
+      
       t.timestamps
     end
   end
